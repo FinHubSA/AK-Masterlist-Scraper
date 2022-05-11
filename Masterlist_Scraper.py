1a33223907b8cc646e2c0b9ead2d727835e28a90
DanaeBouwer <danae.bouwer@gmail.com>
2022-05-11 13:33:14 +0200

    merged solvers

--- Masterlist_Scraper.py
+++ Masterlist_Scraper.py
@@ -1,9 +1,6 @@
 import json
-import os
-import sys
 from pathlib import Path
 import time
-import pandas as pd
 import random
 from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
@@ -12,25 +9,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 from json import JSONDecoder
-import stem.process
 from stem import Signal
 from stem.control import Controller
 import requests
 from datetime import datetime
 import re
+from ast import Not
+import os.path
+
+from recaptcha_solver import recaptcha_solver, delay
+from connection_controllers.gen_connection_controller import GenConnectionController
 from recaptcha_solver import recaptcha_solver
+from temp_storage import storage
 
-# Improve the scraper by making it more human like. This includes:
-# - Random time intervals between clicking on objects
-# - Accessing other web-pages between the website you are scraping
-# - Randomising the search pattern
 
-# Define function to wait
-def delay():
-    waiting_time = random.randrange(5,30,1)
-    driver.implicitly_wait(waiting_time)
+# Set the storage location
+directory = storage.createTempStorage()
 
-# Define function that rotates IP using Tor
+# Define function that rotates IP using Tor <--do not use this now...it does not work well
 def rotateIP():
         print ("Rotating IP")
         with Controller.from_port(port = 9051) as controller:
@@ -67,71 +63,110 @@ while True:
     chrome_options = webdriver.ChromeOptions()
 
     # Add chrome options
+    curdir = Path.cwd().joinpath("BrowserProfile")
     #chrome_options.add_argument(f"--proxy-server=%s" % PROXY)
     chrome_options.add_argument(f"user-agent={USER_AGENT}")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.add_experimental_option("prefs", {
+    "download.default_directory": directory, #Change default directory for downloads
+    "download.prompt_for_download": False, #To auto download the file
+    "download.directory_upgrade": True,
+    "plugins.always_open_pdf_externally": True, #It will not show PDF directly in chrome
+    "credentials_enable_service": False, # gets rid of password saver popup
+    "profile.password_manager_enabled": False #gets rid of password saver popup
+    })
 
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options = chrome_options)         
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options = chrome_options)  
+
+    web_session = GenConnectionController(driver, "https://www.jstor.org")
+    current_url = driver.current_url
 
     # load input file to specify where the loop should start
     with open("start.json","r") as input_file:
         data = json.load(input_file)
-
+    
     start = data['start']
 
-    for x in range(start,100):
+    for x in range(start,200):
+
+        #log start time
+        start_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
         # Article page URL
         URL = f"https://www.jstor.org/stable/{x}"
-        driver.get(URL)
+        driver.get(current_url + "stable/" + URL.split("/")[-1])
 
-        # Accept the cookies
         try:
-            WebDriverWait(driver,20).until(expected_conditions.element_to_be_clickable((By.XPATH, r"//button[@id='onetrust-accept-btn-handler']")))
-            driver.find_element(By.XPATH,r".//button[@id='onetrust-accept-btn-handler']").click()
-            print('cookies accepted')
+            WebDriverWait(driver,20).until(expected_conditions.presence_of_element_located((By.XPATH, r"//main[@id='content']/div/div[@class='error']")))
+            print("[ERR] Error 404: Page not found")
+            
+            # Update tracker file to pin new start location
+            start = x+1
+            data["start"] = start
+            with open("start.json","w") as input_file:
+                json.dump(data, input_file)
+            error=True
         except:
-            print("continue, there aren't any cookies")
+            # Accept the cookies
+            try:
+                WebDriverWait(driver,20).until(expected_conditions.element_to_be_clickable((By.XPATH, r"//button[@id='onetrust-accept-btn-handler']")))
+                driver.find_element(By.XPATH,r".//button[@id='onetrust-accept-btn-handler']").click()
+                print('cookies accepted')
+            except:
+                print("continue, there aren't any cookies")
 
-        # Check for error message
-        try: 
-            WebDriverWait(driver,20).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "error-message")))
-            print("Article page not loading, possible reCAPTCHA")
-            driver.find_element(By.XPATH, r".//content-viewer-pharos-link[@aria-label='Clicking this link will refresh the page.']").click()
-        except:
-            print("No error, continue")
+            # Check for error message
+            try: 
+                WebDriverWait(driver,20).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "error-message")))
+                print("[ERR] Article page not loading, error message and possible reCAPTCHA")
+                driver.find_element(By.XPATH, r".//content-viewer-pharos-link[@aria-label='Clicking this link will refresh the page.']").click()           
+            except:
+                print("No error, continue")
 
-        error=False 
-        try:
-            # Check for reCAPTCHA and Resolve
-            WebDriverWait(driver,20).until(expected_conditions.presence_of_element_located((By.XPATH, "/html/body/div[4]/div[4]/iframe")))
-            print("reCAPTCHA needs to be Resolved")
-            print("Calling reCAPTCHA solver")
-            recaptcha_solver(driver)
-            if recaptcha_solver.error==True:
-                break
-        except:
+            # Check if page has loaded
             try:
                 WebDriverWait(driver,20).until(expected_conditions.presence_of_element_located((By.XPATH, r"//div[@data-qa='stable-url']")))
                 print("page exists")
+                reCAPTCHA_start=""
+                reCAPTCHA_end=""
+                recaptcha_log=""
+                error=False
             except:
+                print("reCAPTCHA needs to be Resolved")    
                 try:
-                    WebDriverWait(driver,20).until(expected_conditions.presence_of_element_located((By.XPATH, r"//main[@id='content']/div/div[@class='error']")))
-                    print("Error 404: Page not found")
+                    # Check for reCAPTCHA and Resolve
+                    WebDriverWait(driver,20).until(expected_conditions.presence_of_element_located((By.XPATH, "/html/body/div[4]/div[4]/iframe")))
                     
-                    # Update tracker file to pin new start location
-                    start = x+1
-                    data["start"] = start
-                    with open("start.json","w") as input_file:
-                        json.dump(data, input_file)
+                    print("Calling reCAPTCHA solver")
+                    
+                    # Declare log variables
+                    reCAPTCHA_start=datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
-                    error=True
-                except:  
-                    print("Unkown error has occured: Resolve to page: " + URL)
-                    break            
+                    # Calling reCAPTCHA solver
+                    recaptcha_solver(driver)
+                    print("returned to program")
 
+                    # Declare log variables
+                    reCAPTCHA_end=datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+                    WebDriverWait(driver,20).until(expected_conditions.presence_of_element_located((By.XPATH, r"//div[@data-qa='stable-url']")))
+                    error=False
+                except:
+                    print("[ERR] Unkown error has occured: restarting driver")
+
+                    #append log file
+                    with open('scraperlog.txt','a+') as log:
+                        log.write('\n')
+                        log.write('\nfor URL: ' + URL)
+                        log.write('\nreCAPTCHA could not be solved and article not scraped: restarted driver') 
+                        log.write('\nscraper started at: ' + start_time)
+                        log.write('\nscraper ended at: ' + '')
+                        log.write('\nreCAPTCHA started at: ' + reCAPTCHA_start)
+                        log.write('\nreCAPTCHA ended at: ' + reCAPTCHA_end)
+                    break
+            
         if error==False:
 
             # Retrieve the metadata
@@ -162,7 +197,7 @@ while True:
             # some articles are rather reviews or comments or replies and will not have abstracts
             abstract=""
             try:
-                WebDriverWait(driver,10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "summary-paragraph")))
+                WebDriverWait(driver,20).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "summary-paragraph")))
                 print("summary found")
                 word = driver.find_element(By.XPATH, r"//div[@class='turn-away-content__article-information']/pharos-heading]").text
                 print(word)
@@ -182,10 +217,10 @@ while True:
                 abstract=None
 
             # 4) Lable article as open access
-            # some articles are opensource and can be freely accessed via JSTOR --> we could download these in this section of code <--
+            # some articles are opensource and can be freely accessed via JSTOR
             open_access=""
             try:  
-                WebDriverWait(driver,30).until(expected_conditions.presence_of_element_located((By.XPATH, r"//div[@id='metadata-info-tab-contents']/div/div/div/div/span/span/span")))
+                WebDriverWait(driver,20).until(expected_conditions.presence_of_element_located((By.XPATH, r"//div[@id='metadata-info-tab-contents']/div/div/div/div/span/span/span")))
                 print("Article is Open Access")
                 open_access=True
             except:
@@ -198,7 +233,7 @@ while True:
                 time.sleep(5)
                 driver.find_element(By.ID, r"reference-tab").click()
                 time.sleep(2)
-                WebDriverWait(driver,30).until(expected_conditions.presence_of_element_located((By.ID, 'reference-tab-contents')))
+                WebDriverWait(driver,20).until(expected_conditions.presence_of_element_located((By.ID, 'reference-tab-contents')))
                 ref=driver.find_element(By.XPATH,r"//div[@id='references']/div/div/ul").text
                 print('references scraped')
             except Exception as e: 
@@ -206,14 +241,74 @@ while True:
                 print('no references in contents')  
         
             
+             # 6) Retrieve author affiliations
+            affiliations=""
+            try:
+                WebDriverWait(driver, 20).until(
+                        expected_conditions.presence_of_element_located((By.CLASS_NAME, 'contrib-group'))
+                        ) 
+                author_info=driver.find_element(By.XPATH,r".//div[@class='contrib-group']//div//span")
+                count=0
+                for item in author_info:
+                    if (count%2) == 0:
+                        affiliations=affiliations+item.text+' - '
+                    else:
+                        affiliations=affiliations+item.text+'. '
+                    count+=1
+                print(affiliations)
+            except:
+                print('no author affiliation') 
+           
+            
+            #7) Download PDF
+            # Construct file name
+            url = os.path.join(directory,URL.split("/")[-1] + ".pdf")
+            doi = os.path.join(directory,"10.2307." + URL.split("/")[-1] + ".pdf")
+
+            if not os.path.isfile(doi): 
+
+                try:
+                    print('finding pdf download')
+                    WebDriverWait(driver,10).until(expected_conditions.presence_of_element_located((By.ID, "metadata-info-tab")))
+                except:    
+                    if WebDriverWait(driver,10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "error-message"))):
+                        print("Article page not loading, possible reCAPTCHA")
+                    
+                        driver.find_element(By.XPATH, r".//content-viewer-pharos-link[@aria-label='Clicking this link will refresh the page.']").click()
+                    
+                    print("Calling reCAPTCHA solver")
+                    recaptcha_solver(driver)
+
+                #click download
+                driver.find_element(by = By.XPATH, value = r".//mfe-download-pharos-button[@data-sc='but click:pdf download']").click()
+                print("clicked on download")
+
+                # bypass t&c (in some case t&c are different, I need to test to find another case again)
+                try:
+                    WebDriverWait(driver, 10).until(
+                        expected_conditions.presence_of_element_located((By.ID, 'content-viewer-container'))
+                        ) 
+                    driver.find_element(by = By.XPATH, value = r".//mfe-download-pharos-button[@data-qa='accept-terms-and-conditions-button']").click()
+                except:
+                    print("no t&c")
+
+                #need to allow time for download to complete and return to initial page
+                time.sleep(30+random.random())
+
+                #rename the pdf file to DOI
+                storage.renameFile(url,doi)
+            else:
+                continue
+
             # Add variables to python dictionary
             metadata = jsmetadatalist[3]
             metadata['Author Name'] = author
             metadata['Abstract'] = abstract
             metadata['References'] = ref
+            metadata['Affiliations'] = affiliations
             metadata['Open Access'] = open_access
             metadata['URL'] = URL
-
+            metadata['Downloaded']="True"
             
             # Update metadata file
             with open('Metadata.json',"r") as file:
@@ -233,7 +328,29 @@ while True:
             with open("start.json","w") as input_file:
                 json.dump(data, input_file)
 
-    driver.close()    
+            # # step 5: delete temp storage folder on local computer --> needs work <--
+            # if storage.countFiles(directory)==15:
+            #     print("deleting downloads")
+            #     storage.deleteTempStorage(directory)
+            # else:
+            #     # try to handle the error: most likely a reCAPTCHA came up and download could not complete
+            #     print("restart the driver")                           
+            
+            # Declare log variables
+            end_time=datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+            #append log file
+            with open('scraperlog.txt','a+') as log:
+                log.write('\n')
+                log.write('\nfor URL: ' + URL)
+                log.write('\ndriver was not restarted') 
+                log.write('\nscraper started at: ' + start_time)
+                log.write('\nscraper ended at: ' + end_time)
+                log.write('\nreCAPTCHA started at: ' + reCAPTCHA_start)
+                log.write('\nreCAPTCHA ended at: ' + reCAPTCHA_end)
+
+    driver.close()
+    delay()
 
 
 
--- recaptcha_solver.py
+++ recaptcha_solver.py
@@ -1,4 +1,3 @@
-from itertools import count
 import time
 import random
 import os.path
@@ -11,151 +10,143 @@ import urllib
 import pydub
 import speech_recognition as sr
 from selenium.webdriver.common.keys import Keys
-from selenium import webdriver
 
-class recaptcha_solver:
+def delay():
+    waiting_time = random.randrange(5,15,1)
+    time.sleep(waiting_time)
+                       
+def recaptcha_solver(driver):
 
-    def __init__(self, driver: webdriver):
+    recaptcha_log=0
 
-        self._driver = driver
+    while True:
+        recaptcha_log=recaptcha_log+1
+        randint=random.randrange(4,8)
 
-        def delay(waiting_time=random.randrange(5,30,1)):
-            driver.implicitly_wait(waiting_time)
+        if recaptcha_log>=randint:
+            print("Too many iterations, restart driver")
+            break
 
-        count=0
-        while True:
-            error=False
-            count=count+1
-            print(count)
-            randomint= random.randrange(3,6)
-            print(randomint)
-            if count >= randomint:
-                delay()
-                driver.get(driver.current_url)
-                count=0
-                delay()
-                driver.navigate().refresh()
+        try:
+            print("trying to find reCAPTCHA")
+            WebDriverWait(driver,20).until(expected_conditions.presence_of_element_located((By.XPATH, r"//div/iframe")))
+            frames = driver.find_elements(By.TAG_NAME,"iframe")
 
-            try:
-                WebDriverWait(driver,30).until(expected_conditions.presence_of_element_located((By.XPATH, r"//div/iframe")))
-                frames = driver.find_elements(By.TAG_NAME,"iframe")
+            recaptcha_control_frame = None
+            recaptcha_challenge_frame = None
 
-                recaptcha_control_frame = None
-                recaptcha_challenge_frame = None
+            for index,frame in enumerate(frames):
+                # Find the reCAPTCHA checkbox
+                if re.search('reCAPTCHA', frame.get_attribute("title")):
+                    recaptcha_control_frame = frame
+                    print('recaptcha box located')
+                # Find the reCAPTCHA puzzle    
+                if re.search('recaptcha challenge expires in two minutes', frame.get_attribute("title")):
+                    recaptcha_challenge_frame = frame
+                    print('recaptcha puzzle located')
 
-                for index,frame in enumerate(frames):
-                    # Find the reCAPTCHA checkbox
-                    if re.search('reCAPTCHA', frame.get_attribute("title")):
-                        recaptcha_control_frame = frame
-                        print('recaptcha box located')
-                    # Find the reCAPTCHA puzzle    
-                    if re.search('recaptcha challenge expires in two minutes', frame.get_attribute("title")):
-                        recaptcha_challenge_frame = frame
-                        print('recaptcha puzzle located')
-                if not (recaptcha_control_frame or recaptcha_challenge_frame):
-                    print("[ERR] Unable to find recaptcha.")
-                    error=False
-                    break
-                
-                # switch to checkbox
-                delay()
-                frames = driver.find_elements(By.TAG_NAME,"iframe")
-                driver.switch_to.frame(recaptcha_control_frame)
-
-                # click on checkbox to activate recaptcha
-                driver.find_element(By.CLASS_NAME,"recaptcha-checkbox-border").click()
-
-                print("checkbox clicked")
-
-                time.sleep(10)
+            if not (recaptcha_control_frame or recaptcha_challenge_frame):
+                print("[ERR] Unable to find recaptcha.")
+                break
             
-            except:
-                print("An error has occured.")
-                print("IP address might have been blocked for recaptcha.")
-                error=True
-                break
-  
+            # switch to checkbox
+            delay()
+            frames = driver.find_elements(By.TAG_NAME,"iframe")
+            driver.switch_to.frame(recaptcha_control_frame)
+
+            # click on checkbox to activate recaptcha
+            driver.find_element(By.CLASS_NAME,"recaptcha-checkbox-border").click()
+
+            print("checkbox clicked")
+
+            time.sleep(10)
+        
+        except:
+            print("An error has occured.")
+            print("IP address might have been blocked for recaptcha.")
+            break
+
+        try:
+            print("trying to find JSTOR page")               
+            WebDriverWait(driver,20).until(expected_conditions.presence_of_element_located((By.XPATH, r"//div[@data-qa='stable-url']")))
+            print("ReCAPTCHA successfully solved after the checkbox was clicked")
+            break
+
+        except:      
             try:
-                print("trying to find JSTOR page")               
-                WebDriverWait(driver,20).until(expected_conditions.presence_of_element_located((By.XPATH, r"//div[@data-qa='stable-url']")))
-                print("ReCAPTCHA successfully solved after the checkbox was clicked")
-                break
-            except:      
+                # switch to recaptcha audio control frame
+                delay()
+                driver.switch_to.default_content()
+                frames = driver.find_elements(By.TAG_NAME,"iframe")
+                driver.switch_to.frame(recaptcha_challenge_frame)
+
+                # click on audio challenge
+                delay()
+                driver.find_element(By.ID,"recaptcha-audio-button").click()
+                print("Switched to audio control frame")
+                
+                #count_puzzle_solver=True
+
+                # switch to recaptcha audio challenge frame
+                driver.switch_to.default_content()
+                frames = driver.find_elements(By.TAG_NAME,"iframe")
+                driver.switch_to.frame(recaptcha_challenge_frame)
+
+                # get the mp3 audio file
+                delay()
+                src = driver.find_element(By.ID,"audio-source").get_attribute("src")
+                print(f"[INFO] Audio src: {src}")
+
+                path_to_mp3 = os.path.normpath(os.path.join(os.getcwd(), "sample.mp3"))
+                path_to_wav = os.path.normpath(os.path.join(os.getcwd(), "sample.wav"))
+
+                # download the mp3 audio file from the source
+                urllib.request.urlretrieve(src, path_to_mp3)
+
+                # load downloaded mp3 audio file as .wav
                 try:
-                    # switch to recaptcha audio control frame
-                    delay()
-                    driver.switch_to.default_content()
-                    frames = driver.find_elements(By.TAG_NAME,"iframe")
-                    driver.switch_to.frame(recaptcha_challenge_frame)
+                    sound = pydub.AudioSegment.from_mp3(path_to_mp3)
+                    sound.export(path_to_wav, format="wav")
+                    sample_audio = sr.AudioFile(path_to_wav)
+                    print("Exported audio file to .wav")
+                except Exception:
+                    print(
+                        "[ERR] Please run program as administrator or download ffmpeg manually, "
+                        "https://blog.gregzaal.com/how-to-install-ffmpeg-on-windows/"
+                        )
 
-                    # click on audio challenge
-                    time.sleep(10)
-                    driver.find_element(By.ID,"recaptcha-audio-button").click()
-                    print("Switched to audio control frame")
-                    
-                    #count_puzzle_solver=True
-
-                    # switch to recaptcha audio challenge frame
-                    driver.switch_to.default_content()
-                    frames = driver.find_elements(By.TAG_NAME,"iframe")
-                    driver.switch_to.frame(recaptcha_challenge_frame)
-
-                    # get the mp3 audio file
-                    delay()
-                    src = driver.find_element(By.ID,"audio-source").get_attribute("src")
-                    print(f"[INFO] Audio src: {src}")
-
-                    path_to_mp3 = os.path.normpath(os.path.join(os.getcwd(), "sample.mp3"))
-                    path_to_wav = os.path.normpath(os.path.join(os.getcwd(), "sample.wav"))
-
-                    # download the mp3 audio file from the source
-                    urllib.request.urlretrieve(src, path_to_mp3)
-
-                    # load downloaded mp3 audio file as .wav
+                # translate audio to text with google voice recognition
+                delay()
+                r = sr.Recognizer()
+                with sample_audio as source:
+                    audio = r.record(source)
                     try:
-                        sound = pydub.AudioSegment.from_mp3(path_to_mp3)
-                        sound.export(path_to_wav, format="wav")
-                        sample_audio = sr.AudioFile(path_to_wav)
-                        print("Exported audio file to .wav")
-                    except Exception:
-                        print(
-                            "[ERR] Please run program as administrator or download ffmpeg manually, "
-                            "https://blog.gregzaal.com/how-to-install-ffmpeg-on-windows/"
-                            )
-
-                    # translate audio to text with google voice recognition
-                    delay()
-                    r = sr.Recognizer()
-                    with sample_audio as source:
-                        audio = r.record(source)
-                        try:
-                            key = r.recognize_google(audio)
-                            print(f"[INFO] Recaptcha Passcode: {key}")
-                            print("Audio Snippet was recognised")
-                        except:
-                            print("reCAPTCHA voice segment is too difficult to solve.")
-                            error=True
-                            break
-
-                    # key in results and submit
-                    delay()
-                    try:
-                        driver.find_element(By.ID,"audio-response").send_keys(key.lower())
-                        driver.find_element(By.ID,"audio-response").send_keys(Keys.ENTER)
-                        time.sleep(5)
-                        driver.switch_to.default_content()
-                        time.sleep(5)
-                        print("Audio snippet submitted")
+                        key = r.recognize_google(audio)
+                        print(f"[INFO] Recaptcha Passcode: {key}")
+                        print("Audio Snippet was recognised")
                     except:
-                        print("An error has occured.")
-                        print("IP address might have been blocked for recaptcha.")
-                        error=True
+                        print("reCAPTCHA voice segment is too difficult to solve.")
                         break
+
+                # key in results and submit
+                delay()
+                try:
+                    driver.find_element(By.ID,"audio-response").send_keys(key.lower())
+                    driver.find_element(By.ID,"audio-response").send_keys(Keys.ENTER)
+                    time.sleep(5)
+                    driver.switch_to.default_content()
+                    time.sleep(5)
+                    print("Audio snippet submitted")
                 except:
-                    print("recurring checkbox")
-                    continue
-        
+                    print("An error has occured.")
+                    print("IP address might have been blocked for recaptcha.")
+                    break
+            except:
+                print("recurring checkbox")
+                continue
 
-        
 
-           
+
+
+
+    
